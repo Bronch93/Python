8f09132b7f32e03ca77166584da6b80978231cc7
Evgeniy <122757239+Bronch93@users.noreply.github.com>
2023-08-21 02:27:59 +0300

    modified:   main.py

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-print("hello")
-print("Result: ", 12, 14,".", sep="")
+# print("Result: ", 12, 14, sep="", end="!\n")
+# print("hello \" \t world") -- доп символы и табуляция
+# print("Result:", min(6, 3, 23, -43, 2), max(6, 3, 23, -43, 2)) -- мин и макс значения
+# print("Result:", abs(-4)) -- модуль числа
+# print("Result:", pow(4, 3)) -- степень числа
+# print("Result:", round(8 / 3)) -- округление числа
+input("age: ")
